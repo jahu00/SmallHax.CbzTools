e0124855274e490e848c33d5cae910d709cd8a4b
jahu00 <jahu.00@gmail.com>
2025-07-02 19:39:45 +0200

    Using relative path in archive
    - Sorting directories alphabetically

--- bulk_convert.py
+++ bulk_convert.py
@@ -24,7 +24,12 @@ for folder in subfolders:
     src_path = os.path.join(args.path, folder)
     dst_path = os.path.join(dst, dst_name)
     items.append((src_path, dst_path))
-    print(folder + "->" + dst_name)
+
+# sort items by source path
+items.sort(key=lambda x: x[0])
+
+for item in items:
+    print(item[0] + "->" + item[1])
 
 if not args.confirmn:
     confirm = input("Apply operations? (y/n) ").lower() == "y"
@@ -37,7 +42,7 @@ for item in items:
     with ZipFile(item[1], "w") as zf:
         for root, dirs, files in os.walk(item[0]):
             for file in files:
-                zf.write(os.path.join(root, file))
+                zf.write(os.path.join(root, file), arcname=file)
                 
     print("Created " + item[1])
     
